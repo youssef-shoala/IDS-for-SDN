5b723a0561dbf48a2d975adb53f51ebb2531118e
Youssef Shoala <youssefshoala@hotmail.com>
2023-12-14 14:22:08 -0500

    removed test and created new SDNcontroller frame

--- SDNcontroller.py
+++ SDNcontroller.py
@@ -1,37 +1,25 @@
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
-from ryu.ofproto import ofproto_v1_3
-from ryu.lib.packet import packet
-from ryu.lib.packet import ethernet
-from ryu.lib.packet import ether_types
-
-from ryu.lib.packet import in_proto
-from ryu.lib.packet import ipv4
-from ryu.lib.packet import icmp
-from ryu.lib.packet import tcp
-from ryu.lib.packet import udp
+from ryu.ofproto import ofproto_v1_0
 
 class L2Switch(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION]
+
     def __init__(self, *args, **kwargs):
         super(L2Switch, self).__init__(*args, **kwargs)
-        self.flow_table = {}
-
-    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
-    def switch_features_handler(self, ev):
-    msg = ev.msg
-
-    self.logger.debug('OFPSwitchFeatures received: '
-                    'datapath_id=0x%016x n_buffers=%d '
-                    'n_tables=%d auxiliary_id=%d '
-                    'capabilities=0x%08x',
-                    msg.datapath_id, msg.n_buffers, msg.n_tables,
-                    msg.auxiliary_id, msg.capabilities)
-
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
-    def _packet_in_handler(self, ev):
+    def packet_in_handler(self, ev):
         msg = ev.msg
-        pkt = packet.Packet(msg.data)
+        dp = msg.datapath
+        ofp = dp.ofproto
+        ofp_parser = dp.ofp
+
+        data = None
+        if msg.buffer_id == ofp.OFP_NO_BUFFER:
+            data = msg.data
+
         self.logger.debug('Packet recieved: ', pkt)
+        pkt = packet.Packet(data)

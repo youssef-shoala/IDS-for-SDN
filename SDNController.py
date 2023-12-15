fd2f4cb36318189c6cd7f04bc4e27e2cb207c0e1
Youssef Shoala <youssefshoala@hotmail.com>
2023-12-15 16:17:51 -0500

    well commented switch and controller py files still working on implementation

--- SDNController.py
+++ SDNController.py
@@ -22,7 +22,7 @@ class SDNController(SimpleSwitch13):
         self.model.load_state_dict(torch.load('dnn_discriminator_real_data'))
         self.model.eval()
         self.logger.debug('model loaded', self.model)
-        print('print statement')
+        print(f'model loaded: {self.model}')
 
 
 
@@ -32,11 +32,11 @@ class SDNController(SimpleSwitch13):
         datapath = ev.datapath
         if ev.state == MAIN_DISPATCHER:
             if datapath.id not in self.datapaths:
-                #self.logger.debug('register datapath: %016x', datapath.id)
+                self.logger.debug('register datapath: %016x', datapath.id)
                 self.datapaths[datapath.id] = datapath
         elif ev.state == DEAD_DISPATCHER:
             if datapath.id in self.datapaths:
-                #self.logger.debug('unregister datapath: %016x', datapath.id)
+                self.logger.debug('unregister datapath: %016x', datapath.id)
                 del self.datapaths[datapath.id]
 
     def _monitor(self):
@@ -46,7 +46,7 @@ class SDNController(SimpleSwitch13):
             hub.sleep(10)
 
     def _request_stats(self, datapath):
-        #self.logger.debug('send stats request: %016x', datapath.id)
+        self.logger.debug('send stats request: %016x', datapath.id)
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
@@ -59,17 +59,22 @@ class SDNController(SimpleSwitch13):
     @set_ev_cls(ofp_event.EventOFPFlowStatsReply, MAIN_DISPATCHER)
     def _flow_stats_reply_handler(self, ev):
         body = ev.msg.body
-        print('new FLOW stats reply msg body: ')
+        print('Packet IN RECIEVED: ')
         print('===============================================================')
-        print(body)
+        #print(body)
+        print(type(ev))
+        print((ev.msg))
+        print((ev.msg.body))
         print('===============================================================')
-
 
 
         # Features Needed: 
         # protocol_type
+        protocol_dict = {'tcp': 0, 'udp': 1, 'icmp': 2} 
         # service
+        service_dict = {'ftp_data': 0, 'other': 1, 'private': 2, 'http': 3, 'remote_job': 4, 'name': 5, 'netbios_ns': 6, 'eco_i': 7, 'mtp': 8, 'telnet': 9, 'finger': 10, 'domain_u': 11, 'supdup': 12, 'uucp_path': 13, 'Z39_50': 14, 'smtp': 15, 'csnet_ns': 16, 'uucp': 17, 'netbios_dgm': 18, 'urp_i': 19, 'auth': 20, 'domain': 21, 'ftp': 22, 'bgp': 23, 'ldap': 24, 'ecr_i': 25, 'gopher': 26, 'vmnet': 27, 'systat': 28, 'http_443': 29, 'efs': 30, 'whois': 31, 'imap4': 32, 'iso_tsap': 33, 'echo': 34, 'klogin': 35, 'link': 36, 'sunrpc': 37, 'login': 38, 'kshell': 39, 'sql_net': 40, 'time': 41, 'hostnames': 42, 'exec': 43, 'ntp_u': 44, 'discard': 45, 'nntp': 46, 'courier': 47, 'ctf': 48, 'ssh': 49, 'daytime': 50, 'shell': 51, 'netstat': 52, 'pop_3': 53, 'nnsp': 54, 'IRC': 55, 'pop_2': 56, 'printer': 57, 'tim_i': 58, 'pm_dump': 59, 'red_i': 60, 'netbios_ssn': 61, 'rje': 62, 'X11': 63, 'urh_i': 64, 'http_8001': 65, 'aol': 66, 'http_2784': 67, 'tftp_u': 68, 'harvest': 69}
         # flag
+        flag_dict = {'SF': 0, 'S0': 1, 'REJ': 2, 'RSTR': 3, 'SH': 4, 'RSTO': 5, 'S1': 6, 'RSTOS0': 7, 'S3': 8, 'S2': 9, 'OTH': 10}
         # count
         # logged_in 
         # serror_rate
@@ -79,6 +84,31 @@ class SDNController(SimpleSwitch13):
         # dst_host_same_srv_rate
         # dst_host_serror_rate
         # dst_host_srv_serror_rate 
+        # classes
+        class_dict = {'normal': 0, 'neptune': 1, 'warezclient': 1, 'ipsweep': 1, 'portsweep': 1, 'teardrop': 1, 'nmap': 1, 'satan': 1, 'smurf': 1, 'pod': 1, 'back': 1, 'guess_passwd': 1, 'ftp_write': 1, 'multihop': 1, 'rootkit': 1, 'buffer_overflow': 1, 'imap': 1, 'warezmaster': 1, 'phf': 1, 'land': 1, 'loadmodule': 1, 'spy': 1, 'perl': 1}
+
+        # Get data from packet_in msg
+        # Get rest of data from controller vars
+        # Predict pkt class based on above data
+        pkt_is_attack = False
+        if pkt_is_attack: 
+            print(f'ATTACK DETECTED   ATTACK DETECTED   ATTACK DETECTED   ATTACK DETECTED   ATTACK DETECTED   ATTACK DETECTED')
+        else:
+            print(f'Benign Traffic')
+        print('===============================================================')
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -97,25 +127,27 @@ class SDNController(SimpleSwitch13):
         #                    stat.instructions[0].actions[0].port,
         #                    stat.packet_count, stat.byte_count)
 
-    @set_ev_cls(ofp_event.EventOFPPortStatsReply, MAIN_DISPATCHER)
-    def _port_stats_reply_handler(self, ev):
-        body = ev.msg.body
-        print('new PORT stats reply msg body: ')
-        print('')
-        print('===============================================================')
-        print(body)
-        print('===============================================================')
-
-        #self.logger.info('datapath         port     '
-        #                'rx-pkts  rx-bytes rx-error '
-        #                'tx-pkts  tx-bytes tx-error')
-        #self.logger.info('---------------- -------- '
-        #                '-------- -------- -------- '
-        #                '-------- -------- --------')
-        #for stat in sorted(body, key=attrgetter('port_no')):
-        #    self.logger.info('%016x %8x %8d %8d %8d %8d %8d %8d',
-        #                    ev.msg.datapath.id, stat.port_no,
-        #                    stat.rx_packets, stat.rx_bytes, stat.rx_errors,
-        #                    stat.tx_packets, stat.tx_bytes, stat.tx_errors)
+#    @set_ev_cls(ofp_event.EventOFPPortStatsReply, MAIN_DISPATCHER)
+#    def _port_stats_reply_handler(self, ev):
+#        body = ev.msg.body
+#        print('new PORT stats reply msg body: ')
+#        print('')
+#        print('===============================================================')
+#        print(type(ev))
+#        print(type(ev.msg))
+#        print(type(ev.msg.body))
+#        print('===============================================================')
+#
+#        #self.logger.info('datapath         port     '
+#        #                'rx-pkts  rx-bytes rx-error '
+#        #                'tx-pkts  tx-bytes tx-error')
+#        #self.logger.info('---------------- -------- '
+#        #                '-------- -------- -------- '
+#        #                '-------- -------- --------')
+#        #for stat in sorted(body, key=attrgetter('port_no')):
+#        #    self.logger.info('%016x %8x %8d %8d %8d %8d %8d %8d',
+#        #                    ev.msg.datapath.id, stat.port_no,
+#        #                    stat.rx_packets, stat.rx_bytes, stat.rx_errors,
+#        #                    stat.tx_packets, stat.tx_bytes, stat.tx_errors)
 
 
--- custom_SimpleSwitch13.py
+++ custom_SimpleSwitch13.py
@@ -57,116 +57,103 @@ class SimpleSwitch13(app_manager.RyuApp):
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
+
+        print('Packet Table MISS: ')
+        print('===============================================================')
+        #print(body)
+        print(type(ev))
+        print((ev.msg))
+        print((ev.msg.body))
+        print('===============================================================')
+
+
+
         # If you hit this you might want to increase
         # the "miss_send_length" of your switch
         if ev.msg.msg_len < ev.msg.total_len:
             self.logger.debug("packet truncated: only %s of %s bytes",
                             ev.msg.msg_len, ev.msg.total_len)
 
+        # get msg data
+
+
+        # Features Needed: 
+        # protocol_type
+        protocol_dict = {'tcp': 0, 'udp': 1, 'icmp': 2} 
+        # service
+        service_dict = {'ftp_data': 0, 'other': 1, 'private': 2, 'http': 3, 'remote_job': 4, 'name': 5, 'netbios_ns': 6, 'eco_i': 7, 'mtp': 8, 'telnet': 9, 'finger': 10, 'domain_u': 11, 'supdup': 12, 'uucp_path': 13, 'Z39_50': 14, 'smtp': 15, 'csnet_ns': 16, 'uucp': 17, 'netbios_dgm': 18, 'urp_i': 19, 'auth': 20, 'domain': 21, 'ftp': 22, 'bgp': 23, 'ldap': 24, 'ecr_i': 25, 'gopher': 26, 'vmnet': 27, 'systat': 28, 'http_443': 29, 'efs': 30, 'whois': 31, 'imap4': 32, 'iso_tsap': 33, 'echo': 34, 'klogin': 35, 'link': 36, 'sunrpc': 37, 'login': 38, 'kshell': 39, 'sql_net': 40, 'time': 41, 'hostnames': 42, 'exec': 43, 'ntp_u': 44, 'discard': 45, 'nntp': 46, 'courier': 47, 'ctf': 48, 'ssh': 49, 'daytime': 50, 'shell': 51, 'netstat': 52, 'pop_3': 53, 'nnsp': 54, 'IRC': 55, 'pop_2': 56, 'printer': 57, 'tim_i': 58, 'pm_dump': 59, 'red_i': 60, 'netbios_ssn': 61, 'rje': 62, 'X11': 63, 'urh_i': 64, 'http_8001': 65, 'aol': 66, 'http_2784': 67, 'tftp_u': 68, 'harvest': 69}
+        # flag
+        flag_dict = {'SF': 0, 'S0': 1, 'REJ': 2, 'RSTR': 3, 'SH': 4, 'RSTO': 5, 'S1': 6, 'RSTOS0': 7, 'S3': 8, 'S2': 9, 'OTH': 10}
+        # count
+        # logged_in 
+        # serror_rate
+        # srv_serrer_rate
+        # same_srv_rate
+        # dst_host_srv_count
+        # dst_host_same_srv_rate
+        # dst_host_serror_rate
+        # dst_host_srv_serror_rate 
+        # classes
+        class_dict = {'normal': 0, 'neptune': 1, 'warezclient': 1, 'ipsweep': 1, 'portsweep': 1, 'teardrop': 1, 'nmap': 1, 'satan': 1, 'smurf': 1, 'pod': 1, 'back': 1, 'guess_passwd': 1, 'ftp_write': 1, 'multihop': 1, 'rootkit': 1, 'buffer_overflow': 1, 'imap': 1, 'warezmaster': 1, 'phf': 1, 'land': 1, 'loadmodule': 1, 'spy': 1, 'perl': 1}
+
+
         msg = ev.msg
         datapath = msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
         in_port = msg.match['in_port']
-
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocols(ethernet.ethernet)[0]
 
-        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
             # ignore lldp packet
+        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
             return
+        
         dst = eth.dst
         src = eth.src
-
         dpid = format(datapath.id, "d").zfill(16)
+
+        # update counters and other self vars
         self.mac_to_port.setdefault(dpid, {})
+        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
 
-        #self.logger.info("packet in fr tho %s %s %s %s", dpid, src, dst, in_port)
-
-        # learn a mac address to avoid FLOOD next time.
+            # learn a mac address to avoid FLOOD next time.
         self.mac_to_port[dpid][src] = in_port
-
         if dst in self.mac_to_port[dpid]:
             out_port = self.mac_to_port[dpid][dst]
         else:
             out_port = ofproto.OFPP_FLOOD
 
+        # define OFP actions
         actions = [parser.OFPActionOutput(out_port)]
 
         # install a flow to avoid packet_in next time
         if out_port != ofproto.OFPP_FLOOD:
 
-            match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
+            # configure packet in 
+            if ethertype:
+                match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
+
+
             # verify if we have a valid buffer_id, if yes avoid to send both
             # flow_mod & packet_out
-            #if msg.buffer_id != ofproto.OFP_NO_BUFFER:
-            #    self.add_flow(datapath, 1, match, actions, msg.buffer_id)
-            #    return
-            #else:
-            #    self.add_flow(datapath, 1, match, actions)
-
-
-
-            
-            # Features Needed: 
-            # protocol_type
-            protocol_dict = {'tcp': 0, 'udp': 1, 'icmp': 2} 
-            # service
-            service_dict = {'ftp_data': 0, 'other': 1, 'private': 2, 'http': 3, 'remote_job': 4, 'name': 5, 'netbios_ns': 6, 'eco_i': 7, 'mtp': 8, 'telnet': 9, 'finger': 10, 'domain_u': 11, 'supdup': 12, 'uucp_path': 13, 'Z39_50': 14, 'smtp': 15, 'csnet_ns': 16, 'uucp': 17, 'netbios_dgm': 18, 'urp_i': 19, 'auth': 20, 'domain': 21, 'ftp': 22, 'bgp': 23, 'ldap': 24, 'ecr_i': 25, 'gopher': 26, 'vmnet': 27, 'systat': 28, 'http_443': 29, 'efs': 30, 'whois': 31, 'imap4': 32, 'iso_tsap': 33, 'echo': 34, 'klogin': 35, 'link': 36, 'sunrpc': 37, 'login': 38, 'kshell': 39, 'sql_net': 40, 'time': 41, 'hostnames': 42, 'exec': 43, 'ntp_u': 44, 'discard': 45, 'nntp': 46, 'courier': 47, 'ctf': 48, 'ssh': 49, 'daytime': 50, 'shell': 51, 'netstat': 52, 'pop_3': 53, 'nnsp': 54, 'IRC': 55, 'pop_2': 56, 'printer': 57, 'tim_i': 58, 'pm_dump': 59, 'red_i': 60, 'netbios_ssn': 61, 'rje': 62, 'X11': 63, 'urh_i': 64, 'http_8001': 65, 'aol': 66, 'http_2784': 67, 'tftp_u': 68, 'harvest': 69}
-            # flag
-            flag_dict = {'SF': 0, 'S0': 1, 'REJ': 2, 'RSTR': 3, 'SH': 4, 'RSTO': 5, 'S1': 6, 'RSTOS0': 7, 'S3': 8, 'S2': 9, 'OTH': 10}
-            # count
-            # logged_in 
-            # serror_rate
-            # srv_serrer_rate
-            # same_srv_rate
-            # dst_host_srv_count
-            # dst_host_same_srv_rate
-            # dst_host_serror_rate
-            # dst_host_srv_serror_rate 
-            # classes
-            class_dict = {'normal': 0, 'neptune': 1, 'warezclient': 1, 'ipsweep': 1, 'portsweep': 1, 'teardrop': 1, 'nmap': 1, 'satan': 1, 'smurf': 1, 'pod': 1, 'back': 1, 'guess_passwd': 1, 'ftp_write': 1, 'multihop': 1, 'rootkit': 1, 'buffer_overflow': 1, 'imap': 1, 'warezmaster': 1, 'phf': 1, 'land': 1, 'loadmodule': 1, 'spy': 1, 'perl': 1}
-
-
-
-            if eth.ethertype == ether_types.ETH_TYPE_IP:
-                    ip = pkt.get_protocol(ipv4.ipv4)
-                    srcip = ip.src
-                    dstip = ip.dst
-                    protocol = ip.proto
-
-                    # if ICMP Protocol
-                    if protocol == in_proto.IPPROTO_ICMP:
-                        t = pkt.get_protocol(icmp.icmp)
-
-                    #  if TCP Protocol
-                    elif protocol == in_proto.IPPROTO_TCP:
-                        t = pkt.get_protocol(tcp.tcp)
-
-                    #  If UDP Protocol
-                    elif protocol == in_proto.IPPROTO_UDP:
-                        t = pkt.get_protocol(udp.udp)
-
-                    match = parser.OFPMatch(eth_type=ether_types.ETH_TYPE_IP,
-                                                ipv4_src=srcip, ipv4_dst=dstip,
-                                                ip_proto=protocol)
-
-                    #self.logger.debug(match)
-                    #print(match)
-
-                    # verify if we have a valid buffer_id, if yes avoid to send both
-                    # flow_mod & packet_out
-                    if msg.buffer_id != ofproto.OFP_NO_BUFFER:
-                        self.add_flow(datapath, 1, match, actions, msg.buffer_id, idle=20, hard=100)
-                        return
-                    else:
-                        self.add_flow(datapath, 1, match, actions)
-
+            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                return
+            else:
+                self.add_flow(datapath, 1, match, actions)
 
         data = None
         if msg.buffer_id == ofproto.OFP_NO_BUFFER:
             data = msg.data
 
+        # send packet_in msg to controller
         out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
                                 in_port=in_port, actions=actions, data=data)
         datapath.send_msg(out)
+
+        print('Packet IN SENDING: ')
+        print('===============================================================')
+        print(out)
+        print('===============================================================')
+

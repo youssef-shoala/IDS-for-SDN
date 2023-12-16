7d5840a6f7e26b5dce245f313e19aa76e557119f
Youssef Shoala <youssefshoala@hotmail.com>
2023-12-15 22:58:46 -0500

    msg

--- custom_SimpleSwitch13.py
+++ custom_SimpleSwitch13.py
@@ -140,11 +140,12 @@ class SimpleSwitch13(app_manager.RyuApp):
 
         # verify if we have a valid buffer_id, if yes avoid to send both
         # flow_mod & packet_out
+        priority = 1
         if msg.buffer_id != ofproto.OFP_NO_BUFFER:
-            self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+            self.add_flow(datapath, priority, match, actions, msg.buffer_id)
             return
         else:
-            self.add_flow(datapath, 1, match, actions)
+            self.add_flow(datapath, priority, match, actions)
 
         data = None
         if msg.buffer_id == ofproto.OFP_NO_BUFFER:
